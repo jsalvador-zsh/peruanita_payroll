8785bf5b85032512c37a0f6dc05bb204898e08dc
jsalvador-zsh <juanstdev@gmail.com>
2025-09-03 21:43:17 -0500

    [ACT] Cambio a False como valor por defecto del SCTR

--- models/hr_contract.py
+++ models/hr_contract.py
@@ -13,6 +13,6 @@ class HrContract(models.Model):
     fifth_category = fields.Float(string='5ta Categoría', digits=(10,2), default=0.0)
     
     # Configuración de seguros
-    has_sctr = fields.Boolean(string='Tiene SCTR', default=True)
+    has_sctr = fields.Boolean(string='Tiene SCTR', default=False)
     sctr_percentage = fields.Float(string='% SCTR', digits=(5,2), default=1.23)
     essalud_percentage = fields.Float(string='% EsSalud', digits=(5,2), default=9.0)
